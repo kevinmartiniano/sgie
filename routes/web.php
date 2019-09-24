e6572010d8bf006c48d0f30cafeb41740b5c9375
kevinmartiniano <kevin.martiniano@hotmail.com>
2019-09-23 21:04:25 -0300

    adding route to login vue.

--- routes/web.php
+++ routes/web.php
@@ -29,17 +29,13 @@ Route::resources([
     ],
 ]);
 
-Route::get('auth/login', 'Auth\AuthController@getLogin');
-Route::post('auth/login', 'Auth\AuthController@postLogin');
-Route::get('auth/logout', 'Auth\AuthController@getLogout');
+// Route::get('auth/login', 'Auth\AuthController@getLogin');
+// Route::post('auth/login', 'Auth\AuthController@postLogin');
+// Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
-// Registration routes...
-Route::get('auth/register', 'Auth\AuthController@getRegister');
-Route::post('auth/register', 'Auth\AuthController@postRegister');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+// // Registration routes...
+// Route::get('auth/register', 'Auth\AuthController@getRegister');
+// Route::post('auth/register', 'Auth\AuthController@postRegister');
 
 Auth::routes();
 
